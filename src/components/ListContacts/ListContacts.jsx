ccad223854c6e10b03d89d1eb313a9a914fdaf89
Enjoy3325 <enjoy3325@gmail.com>
2022-11-22 20:30:47 +0200

    ==

--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -1,31 +1,29 @@
 import { nanoid } from 'nanoid';
 import React from 'react';
 
-export const ListContacts = ({ id, contacts }) => {
-  const serchComplited = contactsId => {
-    this.setState(
-        prevState => ({
-            contacts: prevState.contacts.map(contact => {
-                if (contact.id === contactsId) {
-            return {
-                        ...contact,
-                        id: !contact.id
-                    }
-                }
-          return (
-            <div>
-              <ul>
-                <li id={id}>
-                  <p>{contact}</p>
-                </li>
-              </ul>
-            </div>
-                );
+// export const ListContacts = ({ id, contacts }) => {
+//   const serchComplited = contactsId => {
+//     this.setState(
+//         prevState => ({
+//             contacts: prevState.contacts.map(contact => {
+//                 if (contact.id === contactsId) {
+//             return {
+//                         ...contact,
+//                         id: !contact.id
+//                     }
+//                 }
+//           return (
+//             <div>
+//               <ul>
+//                 <li id={id}>
+//                   <p>{contact}</p>
+//                 </li>
+//               </ul>
+//             </div>
+//                 );
 
-        }
-      })
-    ));
-  };
-};
-
-
+//         }
+//       })
+//     ));
+//   };
+// };
